86aaaca1f0f8f7655a35374ec2015b355588f4ad
Fontella Hooper <hooperfontella6@gmail.com>
2023-09-05 13:53:03 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyMVC.Models;
+
+namespace DojoSurveyMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]  /// #13 showing im about to write a action
+    [Route("")]   // #14 Has a empty base route
+    public ViewResult DojoForm()  // #15writing the function here for the base route
+    {
+        return View("DojoForm");
+    }
+
+
+    [HttpGet("results")]
+    public ViewResult DojoResult()
+    {
+        return View();  //rendering the page
+    }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+
+    [HttpPost("process")]
+    public IActionResult Process(Dojo model)
+    {
+        if (ModelState.IsValid)  // Process and save the data if it's valid
+        {
+
+
+            return View("DojoResult", model);    // You can access model.Name, model.Location, etc. here
+        }
+
+
+        return View("DojoForm", model); // If there are validation errors, render the form again
+    }
+}
---
+++ Models/Dojo.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DojoSurveyMVC.Models;
+
+public class Dojo
+{
+    // Class definition
+    [Required(ErrorMessage = "Name is required.")]
+    [MinLength(2, ErrorMessage = "Name must be at least 2 characters.")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Location is required.")]
+    public string Location { get; set; }
+
+    [Required(ErrorMessage = "Favorite Language is required.")]
+    public string Language { get; set; }
+
+    [MinLength(20, ErrorMessage = "Comment must be at least 20 characters.")]
+    public string Comment { get; set; }
+}
